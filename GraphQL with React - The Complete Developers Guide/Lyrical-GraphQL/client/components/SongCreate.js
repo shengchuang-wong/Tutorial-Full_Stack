9ec59e46ebc13811094ed4c6ea02c5dd16ef61a1
shengchuang-wong <shengchuang1996@gmail.com>
2020-02-23 04:00:18 +0800

    first commit

---
+++ GraphQL with React - The Complete Developers Guide/Lyrical-GraphQL/client/components/SongCreate.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
+
+class SongCreate extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { title: '' }
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.mutate({
+            variables: { title: this.state.title },
+            refetchQueries: [{ query }]
+        }).then(() => {
+            hashHistory.push('/')
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Create a New Song</h3>
+                <Link to="/">Back</Link>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
+                    <input
+                        onChange={e => this.setState({ title: e.target.value })}
+                        value={this.state.title}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+const mutation = gql`
+mutation AddSong($title: String){
+    addSong(title: $title) {
+        title
+    }
+}
+`;
+
+export default graphql(mutation)(SongCreate);
